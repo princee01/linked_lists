d8837924f52cc0562480abc462dea2bd38a30cd9
Prince Upadhyay <upadhyayprince946@gmail.com>
2025-04-05 16:04:35 +0530

    Update 02_insertion_using_recursion.cpp

--- 02_insertion_using_recursion.cpp
+++ 02_insertion_using_recursion.cpp
@@ -1,87 +1,87 @@
 #include <iostream>
 using namespace std;
-// insertion at the end using recursion...........................................................
-//  class node
-//  {
-//  public:
-//      int data;
-//      node *next;
-//      node(int value)
-//      {
-//          data = value;
-//          next = NULL;
-//      }
-//  };
-//  node *createlinkedlist(int arr[], int index, int size)
-//  {
-//      if (index == size)
-//      {
-//          return NULL;
-//      }
-//      node *temp = new node(arr[index]);
-//      temp->next = createlinkedlist(arr, index + 1, size);
+//1. insertion at the end using recursion...........................................................
+ class node
+ {
+ public:
+     int data;
+     node *next;
+     node(int value)
+     {
+         data = value;
+         next = NULL;
+     }
+ };
+ node *createlinkedlist(int arr[], int index, int size)
+ {
+     if (index == size)
+     {
+         return NULL;
+     }
+     node *temp = new node(arr[index]);
+     temp->next = createlinkedlist(arr, index + 1, size);
 
-//     return temp;
-// }
-// int main()
-// {
+    return temp;
+}
+int main()
+{
 
-//     node *head;
-//     head = NULL;
+    node *head;
+    head = NULL;
 
-//     int arr[] = {2, 4, 6, 8};
+    int arr[] = {2, 4, 6, 8};
 
-//     head = createlinkedlist(arr, 0, 4);
+    head = createlinkedlist(arr, 0, 4);
 
-//     node *temp;
-//     temp = head;
-//     while (temp)
-//     {
-//         cout << temp->data << " ";
-//         temp = temp->next;
-//     }
-// }
+    node *temp;
+    temp = head;
+    while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
 
-// insertion at the beginning using recursion.............................................................................
-// class node
-// {
-// public:
-//     int data;
-//     node *next;
-//     node(int value)
-//     {
-//         data = value;
-//         next = NULL;
-//     }
-// };
-// node *fun(int arr[], int index, int size, node *prev)
-// {
-//     if (index == size)
-//     {
-//         return prev;
-//     }
-//     node *temp = new node(arr[index]);
-//     temp->next = prev;
-//     return fun(arr, index + 1, size, temp);
-// }
-// int main()
-// {
+//2insertion at the beginning using recursion.............................................................................
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int value)
+    {
+        data = value;
+        next = NULL;
+    }
+};
+node *fun(int arr[], int index, int size, node *prev)
+{
+    if (index == size)
+    {
+        return prev;
+    }
+    node *temp = new node(arr[index]);
+    temp->next = prev;
+    return fun(arr, index + 1, size, temp);
+}
+int main()
+{
 
-//     node *head;
-//     head = NULL;
+    node *head;
+    head = NULL;
 
-//     int arr[] = {2, 4, 6, 8};
+    int arr[] = {2, 4, 6, 8};
 
-//     head = fun(arr, 0, 4, NULL);
+    head = fun(arr, 0, 4, NULL);
 
-//     node *temp;
-//     temp = head;
-//     while (temp)
-//     {
-//         cout << temp->data << " ";
-//         temp = temp->next;
-//     }
-// }
+    node *temp;
+    temp = head;
+    while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
 
 // 3.insertion at given position...................................................
 class node
